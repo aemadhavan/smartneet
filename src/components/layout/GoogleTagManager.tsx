a990db10f6cd0fa28f04a676d33e5e40aad397a7
Madhavan <enmadhavan@gmail.com>
2025-05-28 21:08:18 +1000

    Refactor Google Tag Manager component to streamline script loading and remove unused code

--- src/components/layout/GoogleTagManager.tsx
+++ src/components/layout/GoogleTagManager.tsx
@@ -8,29 +8,22 @@ import Script from 'next/script';
  */
 
 const GoogleTagManager = () => {
-  //const GTM_ID = 'G-VEF6K86RLG'; // Google Tag Manager ID from the provided script
-const GTM_ID = 'GTM-WVBD7SRF'; 
+  const GTM_ID = 'GTM-WVBD7SRF'; 
   return (
     <>
-      {/* Google Tag Manager - Script that goes in the head */}
+      {/* Google Tag Manager */}
       <Script
         id="gtm-script"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GTM_ID}');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
           `,
         }}
-      />
-      
-      {/* Google Tag Manager - Script for async loading */}
-      <Script
-        id="gtm-script-async"
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
       />
     </>
   );
