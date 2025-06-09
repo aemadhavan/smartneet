3958ee8ed66e27dcfdac3deb9c438e1d38c3828f
aemadhavan <enmadhavan@gmail.com>
2025-06-09 20:42:36 +1000

    Comment out unused import for statsig in flag.ts to clean up code

--- src/components/layout/flag.ts
+++ src/components/layout/flag.ts
@@ -3,7 +3,7 @@ import { statsigAdapter, type StatsigUser } from "@flags-sdk/statsig";
 import { flag, dedupe } from "flags/next";
 import type { Identify } from "flags";
 import { auth } from "@clerk/nextjs/server";
-import { statsig } from "@/lib/statsig";
+//import { statsig } from "@/lib/statsig";
 
 export const identify = dedupe((async () => {
   const { userId } = await auth();
